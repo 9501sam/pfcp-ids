329b014cd29a2be014839a7f680a9a815de91c67
Hsi-Huang Tseng <9501sam@gmail.com>
2023-10-03 19:49:00 +0800

    modify model

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import copy
 import pandas as pd
 import numpy as np
 import torch
@@ -21,21 +23,26 @@ print(DEVICE)
 class Multiclass(nn.Module):
     def __init__(self):
         super().__init__()
-        self.hidden1 = nn.Linear(33, 256)
-        self.hidden2 = nn.Linear(256, 128)  # Add a new hidden layer
-        self.hidden3 = nn.Linear(128, 64)   # Add another hidden layer
+        self.hidden1 = nn.Linear(33, 512)
+        self.hidden2 = nn.Linear(512, 256)
+        self.hidden3 = nn.Linear(256, 128)
+        self.hidden4 = nn.Linear(128, 64)
         self.output = nn.Linear(64, 5)
     
     def forward(self, x):
-        x = F.relu(self.hidden1(x))  # Apply ReLU activation to the first hidden layer
-        x = F.relu(self.hidden2(x))  # Apply ReLU activation to the second hidden layer
-        x = F.relu(self.hidden3(x))  # Apply ReLU activation to the third hidden layer
+        x = F.relu(self.hidden1(x))
+        x = F.relu(self.hidden2(x))
+        x = F.relu(self.hidden3(x))
+        x = F.relu(self.hidden4(x))
         x = self.output(x)
         return x
 
 model = Multiclass().to(DEVICE)
 
-data = pd.read_csv("Balanced PFCP APP Layer/15-sec-CSV/Training/Training_15s.csv")
+data1 = pd.read_csv("Balanced PFCP APP Layer/15-sec-CSV/Training/Training_15s.csv")
+data2 = pd.read_csv("Balanced PFCP APP Layer/15-sec-CSV/Testing/Testing_15s.csv")
+data = pd.concat([data1, data2], axis=0)
+# print(data)
 data = data.drop(columns='flow ID')
 data = data.drop(columns=' destination IP')
 data = data.drop(columns=' source IP')
@@ -57,33 +64,77 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, shuffl
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.0000001)
 
+# prepare model and training parameters
 n_epochs = 1000
-batch_size = 5
-batches_per_epoch = len(X)
-
+batch_size = 10
+batches_per_epoch = len(X_train) // batch_size
+ 
+best_acc = - np.inf   # init to negative infinity
+best_weights = None
+train_loss_hist = []
+train_acc_hist = []
+test_loss_hist = []
+test_acc_hist = []
+ 
+# training loop
 for epoch in range(n_epochs):
+    epoch_loss = []
+    epoch_acc = []
+    # set model in training mode and run through each batch
+    model.train()
     with tqdm.trange(batches_per_epoch, unit="batch", mininterval=0) as bar:
         bar.set_description(f"Epoch {epoch}")
         for i in bar:
             # take a batch
             start = i * batch_size
-            X_batch = X[start:start+batch_size]
-            y_batch = y[start:start+batch_size]
-
-            # Convert one-hot encoded y_batch to class labels
-            y_batch_labels = torch.argmax(y_batch, dim=1)
-
+            X_batch = X_train[start:start+batch_size]
+            y_batch = y_train[start:start+batch_size]
             # forward pass
             y_pred = model(X_batch)
-            loss = loss_fn(y_pred, y_batch_labels)
-
+            loss = loss_fn(y_pred, y_batch)
             # backward pass
             optimizer.zero_grad()
             loss.backward()
-
             # update weights
             optimizer.step()
+            # compute and store metrics
+            acc = (torch.argmax(y_pred, 1) == torch.argmax(y_batch, 1)).float().mean()
+            epoch_loss.append(float(loss))
+            epoch_acc.append(float(acc))
+            bar.set_postfix(
+                loss=float(loss),
+                acc=float(acc)
+            )
+    # set model in evaluation mode and run through the test set
+    model.eval()
     y_pred = model(X_test)
     ce = loss_fn(y_pred, y_test)
     acc = (torch.argmax(y_pred, 1) == torch.argmax(y_test, 1)).float().mean()
-    print(f"Epoch {epoch} validation: Cross-entropy={float(ce)}, Accuracy={float(acc)}")
+    ce = float(ce)
+    acc = float(acc)
+    train_loss_hist.append(np.mean(epoch_loss))
+    train_acc_hist.append(np.mean(epoch_acc))
+    test_loss_hist.append(ce)
+    test_acc_hist.append(acc)
+    if acc > best_acc:
+        best_acc = acc
+        best_weights = copy.deepcopy(model.state_dict())
+    print(f"Epoch {epoch} validation: Cross-entropy={ce:.2f}, Accuracy={acc*100:.1f}%")
+ 
+# Restore best model
+model.load_state_dict(best_weights)
+ 
+# Plot the loss and accuracy
+plt.plot(train_loss_hist, label="train")
+plt.plot(test_loss_hist, label="test")
+plt.xlabel("epochs")
+plt.ylabel("cross entropy")
+plt.legend()
+plt.show()
+ 
+plt.plot(train_acc_hist, label="train")
+plt.plot(test_acc_hist, label="test")
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.legend()
+plt.show()
